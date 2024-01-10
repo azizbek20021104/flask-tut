837a773678a710c455176ddd525ba94841f1206f
djumanov <diyorbekjumanov1120@gmail.com>
2024-01-10 15:36:44 +0500

    url variables

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/api', methods=['POST'])
-def api():
-    values = request.values
-
-    print(values)
-
-    return {"result": 0}
+@app.route('/api/<int:a>/<int:b>', methods=['POST'])
+def api(a: int, b: int):
+    
+    return {"result": a + b}
 
 
 if __name__ == '__main__':
