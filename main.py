c697bd114061ecf560487f5c83a18f5482d58bc7
djumanov <diyorbekjumanov1120@gmail.com>
2024-01-10 15:24:28 +0500

    request json

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route('/api')
 def api():
-    data = json.loads(request.data.decode())
+    data = request.json
 
     return {"result": data['a'] + data['b']}
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
